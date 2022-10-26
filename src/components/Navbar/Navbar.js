107bc4eb3a6418ea599ce490cc00c1f982823c9e
Larppa1 <lari.juva7@gmail.com>
2022-10-26 11:39:48 +0300

    -

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,20 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Navbar(props) {
+    const [menuBtnClicked, setMenuBtnClicked] = useState(false)
+    const [clicked, setClicked] = useState(false)
+
     useEffect(() => {
         const landingLink = document.getElementById('landingLink')
         const rentLink = document.getElementById('rentLink')
         const aboutLink = document.getElementById('aboutLink')
         const contactLink = document.getElementById('contactLink')
 
-        /*Underline nav item corresponding to current page*/
         if(window.innerWidth < 768) { return }
+
+        /*Underline nav item corresponding to current page*/
         switch(props.page) {
             case 'landing':
                 landingLink.style.textDecoration = 'underline';
@@ -29,14 +33,47 @@ export default function Navbar(props) {
 
         /*Animate nav items*/
         landingLink.style.opacity = 1;
-        landingLink.style.transition = 'opacity 0.5s ease-in';
-        rentLink.style.opacity = 1;
-        rentLink.style.transition = 'opacity 1s ease-in';
-        aboutLink.style.opacity = 1;
-        aboutLink.style.transition = 'opacity 1.5s ease-in';
-        contactLink.style.opacity = 1;
-        contactLink.style.transition = 'opacity 2s ease-in';
+        landingLink.style.transition = 'opacity 0.5s ease-in'
+        rentLink.style.opacity = 1
+        rentLink.style.transition = 'opacity 1s ease-in'
+        aboutLink.style.opacity = 1
+        aboutLink.style.transition = 'opacity 1.5s ease-in'
+        contactLink.style.opacity = 1
+        contactLink.style.transition = 'opacity 2s ease-in'
     })
+
+    const dropdownPrimary = () => {
+        if(!menuBtnClicked) {
+            setMenuBtnClicked(true)
+            document.getElementById('dropdown').style.marginRight = '0vw'
+            document.getElementById('dropdown').style.opacity = 1;
+            document.getElementById('dropdown').style.transition = 'opacity 0.2s ease-in-out'
+        }else {
+            document.getElementById('dropdown').style.opacity = 0;
+            document.getElementById('dropdown').style.transition = 'opacity 0.2s ease-in-out'
+            document.getElementById('dropdown').style.transform = 'translate(0vw)'
+            document.getElementById('dropdownSecondary').style.opacity = 0;
+            document.getElementById('dropdownSecondary').style.transition = 'opacity 0.2s ease-in-out'
+            setMenuBtnClicked(false)
+            setClicked(false)
+        }
+    }
+
+    const dropdownSecondary = () => {
+        if(!clicked) {
+            setClicked(true)
+            document.getElementById('dropdown').style.marginRight = '35vw'
+            document.getElementById('dropdown').style.transition = 'margin-right 0.2s ease-in-out'
+            document.getElementById('dropdownSecondary').style.opacity = 1;
+            document.getElementById('dropdownSecondary').style.transition = 'opacity 0.4s ease-in-out'
+        }else {
+            document.getElementById('dropdown').style.marginRight = '0vw'
+            document.getElementById('dropdown').style.transition = 'margin-right 0.2s ease-in-out'
+            document.getElementById('dropdownSecondary').style.opacity = 0;
+            document.getElementById('dropdownSecondary').style.transition = 'opacity 0.4s ease-in-out'
+            setClicked(false)
+        }
+    }
 
     if(window.innerWidth < 768) {
         return(
@@ -45,11 +82,47 @@ export default function Navbar(props) {
                     <Link className="btn btn-ghost text-xl" to='/'>Bergrent</Link>
                 </div>
                 <div className='flex-1'></div>
-                <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <div className="flex-none dropdown dropdown-end">
+                    <button className="btn btn-square btn-ghost" onClick={dropdownPrimary}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </button>
+                    <ul id='dropdown' className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
+                        <li><Link to='/'>Etusivu</Link></li>
+                        <div className="dropdown">
+                            <li tabIndex={0}>
+                                <Link onClick={dropdownSecondary}>Vuokraus</Link>
+                            </li>
+                            <ul id='dropdownSecondary' tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
+                                <li><Link to='/vesijettivuokraus'>Jettivuokraus</Link></li>
+                                <li><Link to='/venevuokraus'>Venevuokraus</Link></li>
+                                <li><Link to='/mokkivuokraus'>Mökkivuokraus</Link></li>
+                            </ul>
+                        </div>
+                        <li><Link to='/tietoja'>Tietoja</Link></li>
+                        <li><Link to='/ota-yhteytta'>Ota yhteyttä</Link></li>
+                    </ul>
                 </div>
+                
+                {/* <div className="flex-none dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </label>
+                    <ul id='dropdown' tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
+                        <li><Link to='/'>Etusivu</Link></li>
+                        <div className="dropdown">
+                            <li tabIndex={0}>
+                                <Link onClick={dropdownSecondary}>Vuokraus</Link>
+                            </li>
+                            <ul id='dropdownSecondary' tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
+                                <li><Link to='/vesijettivuokraus'>Jettivuokraus</Link></li>
+                                <li><Link to='/venevuokraus'>Venevuokraus</Link></li>
+                                <li><Link to='/mokkivuokraus'>Mökkivuokraus</Link></li>
+                            </ul>
+                        </div>
+                        <li><Link to='/tietoja'>Tietoja</Link></li>
+                        <li><Link to='/ota-yhteytta'>Ota yhteyttä</Link></li>
+                    </ul>
+                </div> */}
             </div>
         )
     }else {
