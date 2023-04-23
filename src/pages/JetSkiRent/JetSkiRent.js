bde61cdb5271e839cdc9c7b3ab95c9a31d13f4b8
Larppa1 <lari.juva7@gmail.com>
2023-04-23 20:55:58 +0300

    JetSkiRent stuff

--- src/pages/JetSkiRent/JetSkiRent.js
+++ src/pages/JetSkiRent/JetSkiRent.js
@@ -18,42 +18,48 @@ export default function JetSkiRent() {
                         <h3>Meiltä löytyvät seuraavat vesijetit:</h3>
                     </article>
                     <section style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginBlock: '2%'}}>
-                        <div style={{minHeight: 500, minWidth: '40%', display: 'flex', flexDirection: 'row', backgroundColor: '#FCFCFC', borderRadius: 14}}>
-                            <div style={{minHeight: '100%', minWidth: '60%'}}>
-                                <img />
+                        <div style={{minHeight: 500, width: '40%', display: 'flex', flexDirection: 'row', borderRadius: 14}}>
+                            <div style={{minHeight: '100%', width: '60%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                                <img src='https://i.ibb.co/fYLXr1B/8ddd8336-fa81-46d0-9f5b-48fa6c311cc5.jpg' width='90%' style={{borderRadius: 14}} />
                             </div>
-                            <article className='prose' style={{minHeight: '100%', minWidth: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%', borderLeft: '0.2px solid black'}}>
-                                <h4 style={{textAlign: 'center'}}>2022 Sea Doo Spark Trixx 2UP</h4>
+                            <article className='prose' style={{minHeight: '100%', width: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%', borderLeft: '0.2px solid black'}}>
+                                <h4 style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start'}}>2022 Sea Doo Spark Trixx 2UP</h4>
                                 <div style={{minWidth: '100%'}}>
-                                    <ul>
-                                        <li>Teho: 90hv</li>
-                                        <li>Kulutus: 9l/h</li>
-                                        <li>Istumapaikat: 2</li>
-                                        <li>Trim-ominaisuus: kyllä</li>
-                                    </ul>
+                                    <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start'}}>
+                                        <ul>
+                                            <li>Teho: 90hv</li>
+                                            <li>Kulutus: 9l/h</li>
+                                            <li>Istumapaikat: 2</li>
+                                            <li>Trim-ominaisuus: kyllä</li>
+                                        </ul>
+                                    </div>
+                                    <h4 style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start'}}>Hinnat alkaen 99€</h4>
                                 </div>
                             </article>
                         </div>
-                        <div style={{minHeight: 500, minWidth: '40%', display: 'flex', flexDirection: 'row', backgroundColor: '#FCFCFC', borderRadius: 14}}>
-                            <article className='prose' style={{minHeight: '100%', minWidth: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%', borderRight: '0.2px solid black'}}>
-                                <h4 style={{textAlign: 'center'}}>2022 Sea Doo Spark</h4>
-                                <div style={{minWidth: '100%'}}>
-                                    <ul>
-                                        <li>Teho: 60hv</li>
-                                        <li>Kulutus: 7.34l/h</li>
-                                        <li>Istumapaikat: 2</li>
-                                        <li>Trim-ominaisuus: ei</li>
-                                    </ul>
+                        <div style={{minHeight: 500, width: '40%', display: 'flex', flexDirection: 'row', borderRadius: 14}}>
+                            <article className='prose' style={{minHeight: '100%', width: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%', borderRight: '0.2px solid black'}}>
+                                <h4 style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>2022 Sea Doo Spark</h4>
+                                <div style={{width: '100%'}}>
+                                    <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
+                                        <ul>
+                                            <li>Teho: 60hv</li>
+                                            <li>Kulutus: 7.34l/h</li>
+                                            <li>Istumapaikat: 2</li>
+                                            <li>Trim-ominaisuus: ei</li>
+                                        </ul>
+                                    </div>
+                                    <h4 style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>Hinnat alkaen 89€</h4>
                                 </div>
                             </article>
-                            <div style={{minHeight: '100%', minWidth: '60%'}}>
-                                
+                            <div style={{minHeight: '100%', width: '60%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                                <img src='https://i.ibb.co/QdQj4Qg/e269a6f3-d352-48a4-b962-fb9d3bfe9726.jpg' width='90%' style={{borderRadius: 14}} />
                             </div>
                         </div>
                     </section>
                 </section>
                 <article className='prose' style={{minWidth: '100%', alignItems: 'center', display: 'flex', flexDirection: 'column', marginBottom: '2%'}}>
-                    <p>Varauskalenteri ja lisätietoja löytyy Nettivuokrauksen sivuilta, pääset sinne painamalla <a href='https://www.nettivuokraus.com/vesijetit/vesijetteja-tampere-2kpl/534181/'>tästä.</a></p>
+                    <p>Varauskalenteri ja hinnasto löytyvät Nettivuokrauksen sivuilta, pääset sinne painamalla <a href='https://www.nettivuokraus.com/vesijetit/vesijetteja-tampere-2kpl/534181/'>tästä.</a></p>
                 </article>
             </main>
             <footer>
